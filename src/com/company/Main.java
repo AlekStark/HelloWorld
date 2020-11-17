4da317061f99cba1299debbce30d7d645c170882
AFEDOROVK <afedorovkaritsky@tsconsulting.ru>
2020-11-17 14:37:33 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello Word");
+	System.out.println("Hello World!!!");
+	System.out.println("И тебе привет!!!");
     }
 }
